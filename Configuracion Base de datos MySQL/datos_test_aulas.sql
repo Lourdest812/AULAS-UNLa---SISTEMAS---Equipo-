cec7bca13d3e95de8c97f948b18a0a31095e4435
maubazante <maurobazante3@gmail.com>
2024-04-29 23:02:38 -0300

    -- 1.5.1: Scripts para alta básica de datos de prueba

---
+++ Configuracion Base de datos MySQL/datos_test_aulas.sql
@@ -0,0 +1,25 @@
+# INSERTS AULA
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('1', 'Aula 1 - Scalabrini Ortiz', '30', '0', '1', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('2', 'Aula 2 - Scalabrini Ortiz', '30', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('3', 'Aula 3 - Scalabrini Ortiz', '30', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('4', 'Aula 4 - Scalabrini Ortiz', '30', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('5', 'Aula 5 - Scalabrini Ortiz', '30', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('6', 'Aula 6 - Scalabrini Ortiz', '30', '1', '1', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('7', 'Aula 1 - Arturo Jauretche', '30', '0', '1', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('8', 'Aula 2 - Arturo Jauretche', '30', '1', '0', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('9', 'Aula 3 - Arturo Jauretche', '30', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('10', 'Aula 4 - Arturo Jauretche', '30', '1', '0', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('11', 'Aula 5 - Arturo Jauretche', '30', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('12', 'Aula 6 - Arturo Jauretche', '30', '0', '1', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('13', 'Aula 1 - Manuel Ugarte', '60', '0', '1', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('14', 'Aula 2 - Manuel Ugarte', '60', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('15', 'Aula 3 - Manuel Ugarte', '60', '1', '0', '0');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('16', 'Aula 4 - Manuel Ugarte', '60', '1', '1', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('17', 'Aula 5 - Manuel Ugarte', '60', '1', '1', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('18', 'Aula 6 - Manuel Ugarte', '60', '1', '1', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('19', 'Aula 6 - Manuel Ugarte', '60', '1', '1', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('20', 'Aula 6 - Manuel Ugarte', '60', '1', '0', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('21', 'Aula 6 - Manuel Ugarte', '60', '1', '0', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('22', 'Aula 6 - Manuel Ugarte', '60', '1', '0', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('23', 'Aula 6 - Manuel Ugarte', '60', '1', '0', '1');
+INSERT INTO `proyecto_aula_unla_equipo1`.`classroom` (`idAula`, `name`, `capability`, `hasBlackboard`, `hasProjector`, `hascomputers`) VALUES ('24', 'Aula 6 - Manuel Ugarte', '60', '1', '0', '1');
---
+++ Configuracion Base de datos MySQL/datos_test_materias.sql
@@ -0,0 +1,43 @@
+# INSERT MATERIAS
+
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('1', 'Programación de Computadoras', '1', '60');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('2', 'Organización de Computadoras', '1', '45');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('3', 'Arquitectura de Computadoras', '1', '45');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('4', 'Matemática 1', '1', '45');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('5', 'Matemática 2', '1', '45');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('6', 'Expresión de Problemas y Algoritmos', '1', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('7', 'Introducción al Pensamiento Científico', '1', '25');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('8', 'Arquitectura de Computadoras', '1', '45');
+
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('9', 'Ingeniería de Software 1', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('10', 'Introducción a las Bases de Datos', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('11', 'Algoritmos y Estructuras de Datos', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('12', 'Orientación a Objetos 1', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('13', 'Seminario de Lenguajes', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('14', 'Matemática 2', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('15', 'Matemática 3', '2', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('16', 'Probabilidad y Estadística', '2', '25');
+
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('17', 'Programación Concurrente', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('18', 'Ingeniería de Software 2', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('19', 'Orientación a Objetos 2', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('20', 'Sistemas y organizaciones', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('21', 'Bases de Datos 1', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('22', 'Proyecto de Software', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('23', 'Conceptos y Paradigmas de Lenguajes de Programación', '3', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('24', 'Redes y comunicaciones', '3', '30');
+
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('25', 'Ingeniería de Software 3', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('26', 'Bases de Datos 2', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('27', 'Fundamentos de Teoría de la Computación', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('28', 'Sistemas Operativos', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('29', 'Sistemas Distribuidos', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('30', 'Matemática Discreta', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('31', 'Sistemas Embebidos', '4', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('32', 'Prueba de Software', '4', '30');
+
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('33', 'Política y Gestión Tecnológicas', '5', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('34', 'Aspectos Legales y Profesionales de la Informática', '5', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('35', 'Nuevos Escenarios', '5', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('36', 'Optativa Área Ingeniería de Software y Bases de Datos', '5', '30');
+INSERT INTO `proyecto_aula_unla_equipo1`.`course` (`idcourse`, `subject`, `dictation_year`, `student_limit`) VALUES ('37', 'Seminario de Trabajo Final', '5', '30');
